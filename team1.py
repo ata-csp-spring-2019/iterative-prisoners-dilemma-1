cc735d555ee12d8e8896ab718be00376140d13c9
MEREDITH A SMITH <40504764+merismith@users.noreply.github.com>
2019-03-28 10:39:18 -0400

    Add files via upload

--- team1.py
+++ team1.py
@@ -6,9 +6,15 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+<<<<<<< HEAD
+team_name = 'team01' # Only 10 chars displayed.
+strategy_name = 'alwaysbetrayyourfriends'
+strategy_description = 'alwaysbetray'
+=======
+team_name = 'team1' # Only 10 chars displayed.
+strategy_name = 'alwaysbetrayyourfriends'
+strategy_description = 'alwaysbetray'
+>>>>>>> refs/heads/testingAHBS
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
@@ -19,7 +25,7 @@ def move(my_history, their_history, my_score, their_score):
     '''
 
     # my_history: a string with one letter (c or b) per round that has been played with this opponent.
-    # their_history: a string of the same length as history, possibly empty. 
+    # their_history: a string hahahahaha of the same length as history, possibly empty. 
     # The first round between these two players is my_history[0] and their_history[0].
     # The most recent round is my_history[-1] and their_history[-1].
     
